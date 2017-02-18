dbd71012ab9b51ca64e135e9aaa16b17fac2a238
ajreicha <ajreicha@mtu.edu>
2017-02-18 18:07:37 -0500

    Upload error for Piper to look at

---
+++ src/main/java/com/doughepi/controllers/ProfileController.java
@@ -0,0 +1,20 @@
+package com.doughepi.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by ajreicha on 2/18/17.
+ */
+@Controller
+@RequestMapping(name = "/profile", method = RequestMethod.GET)
+public class ProfileController {
+
+    @RequestMapping
+    public String showProfile() {
+        return "profile";
+    }
+
+}
